0ec4f587deb0b2de3b244e0a496d3f31f9b19587
AmdouniBrahmiKhouloud <khouloud.amdounibrahmi@esprit.tn>
2022-09-28 21:13:59 +0100

    route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+ * Routing simple
+ */
+Route::get('/page1', function () {
+    echo "my first page";
+})->name("page1");
+/*
+ * Routing paramétrés
+ * */
+Route::get('/page2/{name}', function ($name) {
+    echo "<h1> Welcome ".$name. "</h1>";
+})->name("page2");
+Route::get('/page3/{name}/{age}', function ($name,$age) {
+    echo "<h1> Welcome ".$name. " your age is ".$age."</h1>";
+});
+/*
+ * Routing paramétrés optionnel
+ * */
+Route::get('/page4/{name?}', function ($name="user") {
+    echo "<h1> Welcome ".$name. "</h1>";
+});
+/*
+ * Route: Redirection des routes
+ * */
+Route::get('/redirect', function () {
+    return redirect('/page4');
+});
